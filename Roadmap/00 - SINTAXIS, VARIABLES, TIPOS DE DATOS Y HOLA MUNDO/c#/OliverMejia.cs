4c06ffa76e63ab864635e9a15f09507bec6f147d
OliverMejia <oliver.mejia@example.com>
2025-04-15 18:37:09 -0400

    #00 - C#

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c#/OliverMejia.cs
@@ -0,0 +1,38 @@
+using System;
+
+//https://dotnet.microsoft.com/es-es/languages/csharp//
+
+//Formas de Comentar en el lenguaje de programacion csharp//
+
+//Ejemplo de una comentario con una sola linea//
+
+/*Ejemplo de comentario utilizando....
+Varias lineas
+en el mismo editor
+de codigo*/
+
+//variables//
+
+
+class Variables{
+
+       public static void Main(string[] args){
+
+                    int Numero=32;
+                    const int Limite=35;
+
+                    /*Variables de los tipos de datos soportados en 
+                    csharp*/
+
+                    int number= 33; // Numerico Entero por defecto//
+                    string Cadena ="Aprendiendo a programar";//Tipo cadena//
+                    char Caracter='H'; //Caracter//
+                    bool esVerdadero=false; //Booleano//
+                    const string Lenguaje="Cshap";
+
+                    Console.WriteLine("Hola, Csharp");
+
+            }
+
+                    
+}
